8d955e871cd209c2fc5e1cf93a1fcdf88d826ff1
Flavio Vicentini <flaviovicentinijr@gmail.com>
2021-12-10 20:37:34 -0300

    final

---
+++ serverless.ts
@@ -0,0 +1,77 @@
+import createTodo from '@functions/todos/create'
+import showTodo from '@functions/todos/show'
+import type { AWS } from '@serverless/typescript'
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-desafio',
+  frameworkVersion: '2',
+  plugins: [
+    'serverless-esbuild',
+    'serverless-dynamodb-local',
+    'serverless-offline',
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { createTodo, showTodo },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    dynamodb: {
+      stages: ['dev', 'local'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      dbTodos: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          TableName: 'todos',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'id',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            },
+          ],
+        },
+      },
+    },
+  },
+}
+
+module.exports = serverlessConfiguration
